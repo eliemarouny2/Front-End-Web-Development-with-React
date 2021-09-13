5557f8fa29a9ef9bdc2e3b0087c9f9e7f8c0bd58
marouny <elie.marouny12@gmail.com>
2021-09-13 23:18:58 +0300

    fetch handling errors

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -3,6 +3,8 @@ import { Card, CardImg, CardText, CardTitle, Breadcrumb, BreadcrumbItem, Button,
 import { Link } from 'react-router-dom';
 import { Control, Errors, LocalForm, } from 'react-redux-form';
 import { Loading } from './LoadingComponent';
+import { baseUrl } from '../shared/baseUrl';
+
 
 const required = (val) => val && val.length;
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
@@ -103,7 +105,7 @@ function RenderDish({ dish }) {
    return (
       <div className="col-xs-12 col-md-5 m-1">
          <Card>
-            <CardImg src={dish.image} alt="dish name" />
+            <CardImg src={baseUrl + dish.image} alt="dish name" />
             <CardTitle>{dish.name}</CardTitle>
             <CardText>{dish.description}</CardText>
          </Card>
